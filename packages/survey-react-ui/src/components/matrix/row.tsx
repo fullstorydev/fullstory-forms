4cb7627c12207dabfa8dd0c415d5f27155d5d3d3
bateman001 <49342979+bateman001@users.noreply.github.com>
2025-03-17 11:28:01 -0400

    removed some surface element data stuff

--- packages/survey-react-ui/src/components/matrix/row.tsx
+++ packages/survey-react-ui/src/components/matrix/row.tsx
@@ -5,75 +5,70 @@ import { ReactElementFactory } from "../../element-factory";
 import { SurveyElementBase } from "../../reactquestion_element";
 
 interface IMatrixRowProps {
-    model: QuestionMatrixDropdownRenderedRow;
-    parentMatrix: QuestionMatrixDropdownModelBase;
-    elementData?: any;
+  model: QuestionMatrixDropdownRenderedRow;
+  parentMatrix: QuestionMatrixDropdownModelBase;
 }
 
 export class MatrixRow extends SurveyElementBase<IMatrixRowProps, any> {
-    private root: React.RefObject<HTMLTableRowElement> = React.createRef();
-    constructor(props: IMatrixRowProps) {
-        super(props);
-    }
-    get model(): QuestionMatrixDropdownRenderedRow {
-        return this.props.model;
-    }
-    get parentMatrix(): QuestionMatrixDropdownModelBase {
-        return this.props.parentMatrix;
-    }
-    get elementData(): any {
-        return this.props.elementData;
-    }
-    protected getStateElement() {
-        return this.model;
-    }
-    protected onPointerDownHandler = (event: any) => {
-        this.parentMatrix.onPointerDown(event.nativeEvent, this.model.row);
-    };
+  private root: React.RefObject<HTMLTableRowElement> = React.createRef();
+  constructor(props: IMatrixRowProps) {
+    super(props);
+  }
+  get model(): QuestionMatrixDropdownRenderedRow {
+    return this.props.model;
+  }
+  get parentMatrix(): QuestionMatrixDropdownModelBase {
+    return this.props.parentMatrix;
+  }
+  protected getStateElement() {
+    return this.model;
+  }
+  protected onPointerDownHandler = (event: any) => {
+    this.parentMatrix.onPointerDown(event.nativeEvent, this.model.row);
+  };
 
-    componentDidMount(): void {
-        super.componentDidMount();
-        if (this.root.current) {
-            this.model.setRootElement(this.root.current);
-        }
+  componentDidMount(): void {
+    super.componentDidMount();
+    if (this.root.current) {
+      this.model.setRootElement(this.root.current);
     }
+  }
 
-    componentWillUnmount(): void {
-        super.componentWillUnmount();
+  componentWillUnmount(): void {
+    super.componentWillUnmount();
+    this.model.setRootElement(undefined as any);
+  }
+
+  public shouldComponentUpdate(nextProps: any, nextState: any): boolean {
+    if (!super.shouldComponentUpdate(nextProps, nextState)) return false;
+    if (nextProps.model !== this.model) {
+      if (nextProps.element) {
+        nextProps.element.setRootElement(this.root.current);
+      }
+      if (this.model) {
         this.model.setRootElement(undefined as any);
+      }
     }
+    return true;
+  }
 
-    public shouldComponentUpdate(nextProps: any, nextState: any): boolean {
-        if (!super.shouldComponentUpdate(nextProps, nextState)) return false;
-        if (nextProps.model !== this.model) {
-            if (nextProps.element) {
-                nextProps.element.setRootElement(this.root.current);
-            }
-            if (this.model) {
-                this.model.setRootElement(undefined as any);
-            }
-        }
-        return true;
-    }
+  render() {
+    const model = this.model;
+    if (!model.visible) return null;
 
-    render() {
-        const model = this.model;
-        if (!model.visible) return null;
-
-        return (
-            <tr
-                {...this.elementData}
-                ref={this.root}
-                className={model.className}
-                data-sv-drop-target-matrix-row={model.row && model.row.id}
-                onPointerDown={(event: any) => this.onPointerDownHandler(event)}
-            >
-                {this.props.children}
-            </tr>
-        );
-    }
+    return (
+      <tr
+        ref={this.root}
+        className={model.className}
+        data-sv-drop-target-matrix-row={model.row && model.row.id}
+        onPointerDown={(event: any) => this.onPointerDownHandler(event)}
+      >
+        {this.props.children}
+      </tr>
+    );
+  }
 }
 
 ReactElementFactory.Instance.registerElement("sv-matrix-row", (props: IMatrixRowProps) => {
-    return React.createElement(MatrixRow, props);
+  return React.createElement(MatrixRow, props);
 });
